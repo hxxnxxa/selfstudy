d013acfcd09871ffd05c187f57232c9854091049
hxxnxxa <hyunhw92@hanmail.net>
2022-10-30 23:39:44 +0900

    added selfstudy

--- deeplearning-from-scratch/chapter03/ex01_activationEx1.py
+++ deeplearning-from-scratch/chapter03/ex01_activationEx1.py
@@ -8,18 +8,5 @@ def step_function(x):
 x가 실수인 경우 받아들여지지만 numpy 배열형인 경우 인수로 넣기 불가능
 '''
 
-#print(step_function([0.5, 0.5]))
+print(step_function([0.5, 0.5]))
 # TypeError: '>' not supported between instances of 'list' and 'int'
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
-
-x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
-
-plt.plot(x,y)
-plt.ylim(-0.1, 1.1)
-plt.show()
---
+++ deeplearning-from-scratch/chapter03/ex03_sigmoid.py
@@ -0,0 +1,19 @@
+# 시그모이드 함수 그래프
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y1 = sigmoid(x)
+y2 = step_function(x)
+
+plt.plot(x,y1,label="sigmoid")
+plt.plot(x,y2, linestyle="--", label="step")
+plt.ylim(-0.1, 1.1)
+plt.show()
