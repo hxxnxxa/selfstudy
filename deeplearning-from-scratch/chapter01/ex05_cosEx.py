30d4a40fff6191557a616b2dcd3b5ceaf922a24c
hxxnxxa <hyunhw92@hanmail.net>
2022-10-30 22:28:15 +0900

    added selfstudy

--- deeplearning-from-scratch/chapter01/ex05_cosEx.py
+++ deeplearning-from-scratch/chapter01/ex05_cosEx.py
@@ -7,5 +7,10 @@ y1 = np.sin(x)
 y2 = np.cos(x)
 
 # 그래프 그리기 
-plt.plot(x, y1, lable="sin")
-plt.plot(x,)
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label='cos')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("sin & cos")
+plt.legend()
+plt.show()
