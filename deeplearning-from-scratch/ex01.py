90509b070f3cb103299cc77d258c9969aca13fbe
hxxnxxa <hyunhw92@hanmail.net>
2022-10-30 18:51:08 +0900

    added selfstudy

--- deeplearning-from-scratch/ex01.py
+++ deeplearning-from-scratch/ex01.py
@@ -1,10 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
+'''
 
-# 데이터 준비
-x = np.arange(0, 6, 0.1) #0에서 6까지 0.1 간격으로 생성
-y = np.sin(x)
+    chapter 1. 헬로 파이썬
 
-# 그래프 그리기
-plt.plot(x, y)
-plt.show()
+'''
