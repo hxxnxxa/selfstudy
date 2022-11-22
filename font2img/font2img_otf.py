ef35bdb7bfe8d82a4f918dd4dc718691e33822c8
hxxnxxa <ohyunwha@gmail.com>
2022-11-22 13:17:16 +0900

    added research 221122

---
+++ font2img/font2img_otf.py
@@ -0,0 +1,42 @@
+# ttf 폰트 적용
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFont, ImageDraw
+from numpy.core.arrayprint import TimedeltaFormat
+from tqdm import tqdm
+import IPython.display as display
+from matplotlib import font_manager
+import typer
+
+# example : python font2img_3.py 12_Noto_Sans_KR/NotoSansKR-Black AD81 NotoSansKR-Black
+
+#def font2img(ttf_path:str, uni: str, new_path: str): 
+def font2img(ttf_path:str, uni: str, new_path: str):
+    font_path = otf_path #12_Noto_Sans_KR/NotoSansKR-Black
+    tgt_path  = new_path #NotoSansKR-Black
+    os.makedirs(tgt_path, exist_ok=True)
+
+    unicodeChars = chr(int(uni, 16))
+    print(unicodeChars)
+
+    w, h = (256, 256)
+
+    font = ImageFont.truetype(font_path+".otf", size=200)
+    x, y = font.getsize(chr(int(uni, 16)))
+    print("x:",x)
+    print("y:",y)
+
+    theImage = Image.new('RGB', (256,256),color='white')
+    theDrawPad = ImageDraw.Draw(theImage)
+
+    theDrawPad.text(((w-x)/2,-20), unicodeChars, font=font, fill='black') #폰트 타입이 OTF인 경우에 센터값 맞추려면 이 코드 이용
+
+    msg = tgt_path + "/" + new_path + "_" + uni + "_origin"
+    print(msg)
+    theImage.save('{}.png'.format(msg))
+
+if __name__ == "__main__":
+    typer.run(font2img)
---
+++ font2img/font2img_ttf.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFont, ImageDraw
+from numpy.core.arrayprint import TimedeltaFormat
+from tqdm import tqdm
+import IPython.display as display
+from matplotlib import font_manager
+import typer
+
+# example : python font2img_3.py 1_Gothic/08SeoulNamsanB AD81 08SeoulNamsanB
+
+def font2img(ttf_path:str, uni: str, new_path: str): 
+
+    font_path = "D:/workspace/SF-GNN/codeHistory/fonts/" + ttf_path #1_Gothic/08SeoulNamsanB
+    tgt_path  = "D:/workspace/SF-GNN/codeHistory/fontImage/origin/" + new_path #08SeoulNamsanB
+    os.makedirs(tgt_path, exist_ok=True)
+
+    unicodeChars = chr(int(uni, 16))
+    print(unicodeChars)
+
+    w, h = (256, 256)
+
+    font = ImageFont.truetype(font_path+".ttf", size=200)
+    x, y = font.getsize(chr(int(uni, 16)))
+    print("x:",x)
+    print("y:",y)
+
+    
+    theImage = Image.new('RGB', (256,256),color='white')
+    theDrawPad = ImageDraw.Draw(theImage)
+    #theDrawPad.text(((w-x)/2, (h-y)/2), unicodeChars, font=font, fill='black')
+    #theDrawPad.text(((w-x)/2, -20), unicodeChars, font=font, fill='black')
+    theDrawPad.text((70,0), unicodeChars, font=font, fill='black') # Aunt-freedom 센터값
+
+    msg = tgt_path + "/" + new_path + "_" + uni + "_origin"
+    print(msg)
+    theImage.save('{}.png'.format(msg))
+
+if __name__ == "__main__":
+    typer.run(font2img)
